4b0ea31626bb4492ba768186581a66bebef317ab
dipanshkhandelwal <deepanshrocks438@gmail.com>
2017-07-15 19:53:01 +0530

    Overrided onConnectionFailed method

--- app/src/main/java/com/example/dipanshkhandelwal/location/MainActivity.java
+++ app/src/main/java/com/example/dipanshkhandelwal/location/MainActivity.java
@@ -2,11 +2,13 @@ package com.example.dipanshkhandelwal.location;
 
 import android.content.pm.PackageManager;
 import android.location.Location;
+import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
 
+import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.location.LocationListener;
 import com.google.android.gms.location.LocationRequest;
@@ -72,5 +74,10 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.C
     @Override
     public void onConnectionSuspended(int i) {
         mGoogleApiClient.connect();
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
     }
 }
