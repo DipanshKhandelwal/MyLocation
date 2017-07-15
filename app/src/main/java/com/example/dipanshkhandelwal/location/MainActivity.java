0ee293e27aef29e1ed31f5b57b190878cf7ea0f7
dipanshkhandelwal <deepanshrocks438@gmail.com>
2017-07-15 19:33:28 +0530

    main activity implements the GoogleApiClient activities

--- app/src/main/java/com/example/dipanshkhandelwal/location/MainActivity.java
+++ app/src/main/java/com/example/dipanshkhandelwal/location/MainActivity.java
@@ -3,7 +3,11 @@ package com.example.dipanshkhandelwal.location;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.common.api.GoogleApiClient;
+
+public class MainActivity extends AppCompatActivity implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener{
+
+    protected static final String TAG = "Location";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
