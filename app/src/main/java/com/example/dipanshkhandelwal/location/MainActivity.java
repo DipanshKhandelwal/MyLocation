a0a514760fd0c23ba2a93f1c4cde77b93d828fb9
dipanshkhandelwal <deepanshrocks438@gmail.com>
2017-07-15 19:37:53 +0530

    Added GoogleApiClient in main activity

--- app/src/main/java/com/example/dipanshkhandelwal/location/MainActivity.java
+++ app/src/main/java/com/example/dipanshkhandelwal/location/MainActivity.java
@@ -1,17 +1,33 @@
 package com.example.dipanshkhandelwal.location;
 
+import android.location.Location;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationServices;
 
 public class MainActivity extends AppCompatActivity implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener{
 
     protected static final String TAG = "Location";
+    protected GoogleApiClient mGoogleApiClient;
+    protected Location mLocationRequest;
+    protected TextView mLatitudeText;
+    protected TextView mLongitudeText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(LocationServices.API)
+                .build();
+
+        mLatitudeText = (TextView) findViewById(R.id.latitude_text);
+        mLongitudeText = (TextView) findViewById(R.id.longitude_text);
     }
 }
